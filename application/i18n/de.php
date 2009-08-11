cb0ee676db6b10d99aa44632519ff04b882468a8
Andrew <easylancer@gmail.com>
2009-08-11 01:51:34 +0100

    Merged Install with Master branch

---
+++ application/i18n/de.php
@@ -0,0 +1,17 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+return array
+(
+	// Login
+	'Username' => 'Benutzername',
+	'Password' => 'Passwort',
+	'Login' => 'Einloggen',
+	'You must enter a username' => 'Sie müssen einen Benutzernamen eingeben',
+	'You must enter a password' => 'Sie müssen ein Passwort eingeben',
+	'Invalid username or password' => 'Benutzername oder Passwort ungültig',
+
+	// Admin
+	'Logged in as %username' => 'Eingeloggt als %username',
+	'Logout' => 'Abmelden',
+	'Visit site' => 'Zur Startseite',
+);
