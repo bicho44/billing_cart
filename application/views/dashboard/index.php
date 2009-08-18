6120e0bbc1995e317e5d78603fa05224e5b4945a
Andrew <easylancer@gmail.com>
2009-08-18 08:42:30 +0100

    still applyting theme to application

--- application/views/dashboard/index.php
+++ application/views/dashboard/index.php
@@ -1,4 +1,9 @@
 <div class="container_16">
-	<h1>Welcome <?php echo $username; ?></h1>
-	<h3><a href="<?php echo url::site('login/logout'); ?>">Logout</a></h3>
+	<div class="grid_13">
+		<h1>Welcome <?php echo $username; ?></h1>
+		<h3><a href="<?php echo url::site('login/logout'); ?>">Logout</a></h3>
+	</div>
+	<div id="sidebar" class="grid_3">
+		<a id="new-invoice" href="<?php echo url::site('invoice/add');; ?>"><span>New Invoice</span></a>
+	</div>
 </div>
